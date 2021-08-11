8e65f63a9a2d52d68a71723ef39a7906dfd0cdf9
Alexander Pietrasch <alexander.pietrasch@springlane.de>
2021-08-11 14:36:10 +0200

    First steps

--- app/app.py
+++ app/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker'
+def main():
+    return render_template('main.html')
+
+
+@app.route('/hello')
+def hello_world2():
+    return 'Hello2'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
